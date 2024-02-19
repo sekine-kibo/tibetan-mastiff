8cc99bc250b7b8bbc7b5619a7587fe6d31a36e17
sekine-kibo <Kibo.S.Hope@gmail.com>
2024-02-19 23:21:38 +0900

    not-found ページを実装

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client"; // 必要？
+
+export function NotFound() {
+  return (
+    <div>
+      <div className="text-center text-5xl font-bold mb-3">404</div>
+      <div className="text-center text-xl font-bold">Not Found</div>
+    </div>
+  );
+}
