de17617786a792dc79e914d5f914f7ba161093f5
sekine-kibo <Kibo.S.Hope@gmail.com>
2024-02-19 23:18:59 +0900

    error ページを実装

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client"; // 必要？
+
+export default function Error() {
+  return (
+    <div>
+      <div className="text-center text-5xl font-bold mb-3">500</div>
+      <div className="text-center text-xl font-bold">Server Error</div>
+    </div>
+  );
+}
