579a9bb3cb8bd4220425e7084c9d435602ff3f89
sekine-kibo <Kibo.S.Hope@gmail.com>
2024-02-19 23:18:30 +0900

    loading ページを実装

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <Loader2 className="h-8 w-8 animate-spin" />
+    </div>
+  );
+}
