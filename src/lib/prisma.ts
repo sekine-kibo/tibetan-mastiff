eb9714b1ba248cd3661c051064fa5401f4f3d291
sekine-kibo <Kibo.S.Hope@gmail.com>
2024-02-20 23:20:38 +0900

    setup NextAuth

---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// PrizmaClient のインスタンスを作成
+const client = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = client;
+}
+
+export default client;
